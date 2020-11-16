b864fce426dc26c7a89279230850070f6927e62e
Maxime St-Hilaire <maximesthilaire1@gmail.com>
2020-11-16 14:52:30 -0500

    Finished

---
+++ controllers/AccountsController.js
@@ -0,0 +1,99 @@
+const Repository = require('../models/Repository');
+const TokenManager = require('../tokenManager');
+const utilities = require("../utilities");
+const User = require('../models/user');
+
+module.exports =
+    class AccountsController extends require('./Controller') {
+        constructor(req, res) {
+            super(req, res);
+            this.usersRepository = new Repository('Users');
+        }
+
+        // list of users with masked password
+        index(id) {
+            if (!isNaN(id)) {
+                let user = this.usersRepository.get(id);
+                if (user != null) {
+                    let userClone = { ...user };
+                    userClone.Password = "********";
+                    this.response.JSON(userClone);
+                }
+            }
+            else {
+                let users = this.usersRepository.getAll();
+                let usersClone = users.map(user => ({ ...user }));
+                for (let user of usersClone) {
+                    user.Password = "********";
+                }
+                this.response.JSON(usersClone);
+            }
+        }
+
+        // POST: /token body payload[{"Email": "...", "Password": "...", "grant-type":"password"}]
+        login(loginInfo) {
+            // to do assure that grant-type is present in the request header
+            let user = this.usersRepository.findByField("Email", loginInfo.Email);
+            if (user != null) {
+                if (user.Password == loginInfo.Password) {
+                    let newToken = TokenManager.create(user.Email);
+                    newToken["UserId"] = user.Id;
+                    newToken["UserName"] = user.Name;
+                    newToken["UserEmail"] = user.Email;
+                    console.log(newToken);
+                    this.response.JSON(newToken);
+                } else
+                    this.response.badRequest();
+            } else
+                this.response.badRequest();
+        }
+
+        // POST: account/register body payload[{"Id": 0, "Name": "...", "Email": "...", "Password": "..."}]
+        register(user) {
+            user.Created = utilities.nowInSeconds();
+            // validate User before insertion
+            if (User.valid(user)) {
+                // avoid duplicates Email
+                if (this.usersRepository.findByField('Email', user.Email) == null) {
+                    // take a clone of the newly inserted user
+                    let newUser = { ...this.usersRepository.add(user) };
+                    if (newUser) {
+                        // mask password in the json object response
+                        newUser.Password = "********";
+                        this.response.created(newUser);
+                    } else
+                        this.response.internalError();
+                } else
+                    this.response.conflict();
+            } else
+                this.response.unprocessable();
+        }
+        
+        change(user) {
+            if (this.requestActionAuthorized()) {
+                if (User.valid(user)) {
+                    let foundUser = this.usersRepository.findByField('Name', user.Name);
+                    if (foundUser != null) {
+                        if (foundUser.Id != user.Id) {
+                            this.response.conflict();
+                            return;
+                        }
+                    }
+                    if (this.usersRepository.update(user))
+                        this.response.ok();
+                    else
+                        this.response.notFound();
+                } else
+                    this.response.unprocessable();
+            }
+        }
+        // todo
+        remove(id) {
+            if (this.requestActionAuthorized()) {
+                if (this.usersRepository.remove(id))
+                    this.response.accepted();
+                else
+                    this.response.notFound();
+            }
+        }
+    }
